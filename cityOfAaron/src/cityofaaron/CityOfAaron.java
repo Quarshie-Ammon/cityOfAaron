75d4bd3bbe70c7aee8c78e877dca48d6aa4d65df
raymullins <35316118+raymullins@users.noreply.github.com>
2018-05-18 20:08:07 -0400

    5-18-2018 Added CropData Class with test code in main()

--- cityOfAaron/src/cityofaaron/CityOfAaron.java
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -8,6 +8,7 @@ package cityofaaron;
 import byui.cit260.cityofaaron.model.Location;
 import byui.cit260.cityofaaron.model.Player;
 import byui.cit260.cityofaaron.model.TeamMember;
+import byui.cit260.cityofaaron.model.CropData;
 
 /**
  *
@@ -20,6 +21,7 @@ public class CityOfAaron {
      */
     public static void main(String[] args) {
         Player playerOne = new Player();
+        CropData cropDataTest = new CropData();
         
         playerOne.setName("Aaron");
         
@@ -37,7 +39,46 @@ public class CityOfAaron {
         initialPosition.setSymbol("Shield");
                 
             System.out.println(initialPosition.toString());
+      
+       //CropData class test code
+       
+        cropDataTest.setYear(9);
+        cropDataTest.setPopulation(956);
+        cropDataTest.setAcresOwned(100);
+        cropDataTest.setCropYield(50);
+        cropDataTest.setWheatInStore(40);
+        cropDataTest.setNumberWhoDied(3);
+        cropDataTest.setNewPeople(2);
+        cropDataTest.setHarvest(100);
+        cropDataTest.setHarvestAfterOffering(110);
+        cropDataTest.setOffering(10);
+        cropDataTest.setOfferingBushels(12);
+        cropDataTest.setPeopleFed(1000);
+        cropDataTest.setAcresPlanted(100);
+        cropDataTest.setNumStarved(1);
+        cropDataTest.setEatenByRats(3);
+        cropDataTest.setWheatForPeople(20);
         
+        int cropDataYear = cropDataTest.getYear();
+        int cropDataPopulation = cropDataTest.getPopulation();
+        int cropDataAcresOwned = cropDataTest.getAcresOwned();
+        int cropDatacropYield = cropDataTest.getCropYield();
+        int cropDatawheatInStore = cropDataTest.getWheatInStore();
+        int cropDatanumberWhoDied = cropDataTest.getNumberWhoDied();
+        int cropDataNewPeople = cropDataTest.getNewPeople();
+        int cropDataHarvest = cropDataTest.getHarvest();
+        int cropDataHarvestAfterOffering = cropDataTest.getHarvestAfterOffering();
+        int cropDataOffering = cropDataTest.getOffering();
+        int cropDataOfferingBushels = cropDataTest.getOfferingBushels();
+        int cropDataPeopleFed = cropDataTest.getPeopleFed();
+        int cropDataAcresPlanted = cropDataTest.getAcresPlanted();
+        int cropDataNumStarved = cropDataTest.getNumStarved();
+        int cropDataEatenByRats = cropDataTest.getEatenByRats();
+        int cropDataWheatForPeople = cropDataTest.getWheatForPeople();
+        
+        System.out.println(cropDataTest.toString());
+        
+        // end cropData test code
         
     }
     
