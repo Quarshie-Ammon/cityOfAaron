ecb9871aabe217b3a5ddccfc3514d33aa0c73fd2
ammonknight95 <35312030+ammonknight95@users.noreply.github.com>
2018-05-31 20:59:39 +0000

    adding small changes

---
+++ cityOfAaron/src/byui/cit260/cityofaaron/control/InventoryControl.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.cityofaaron.control;
+import byui.cit260.cityofaaron.model.*;
+ import java.util.Random;
+/**
+ *
+ * @author Saturn
+ */
+public class InventoryControl {
+    
+    public static void buyLand(int landPrice, int acresToBuy, CropData cropData){
+        System.out.println(landPrice + "  " + acresToBuy);
+    }
+    
+}
