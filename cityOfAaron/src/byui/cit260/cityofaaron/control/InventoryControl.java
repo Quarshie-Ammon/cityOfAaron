f912052000031aeedae8a7774ed4cbe890166b60
ammonknight95 <35312030+ammonknight95@users.noreply.github.com>
2018-05-31 21:48:10 +0000

    adding small changes

--- cityOfAaron/src/byui/cit260/cityofaaron/control/InventoryControl.java
+++ cityOfAaron/src/byui/cit260/cityofaaron/control/InventoryControl.java
@@ -5,7 +5,9 @@
  */
 package byui.cit260.cityofaaron.control;
 import byui.cit260.cityofaaron.model.*;
+import static byui.cit260.cityofaaron.model.CropData.calcLandCost;
  import java.util.Random;
+import java.util.Scanner;
 /**
  *
  * @author Saturn
@@ -13,7 +15,24 @@ import byui.cit260.cityofaaron.model.*;
 public class InventoryControl {
     
     public static void buyLand(int landPrice, int acresToBuy, CropData cropData){
-        System.out.println(landPrice + "  " + acresToBuy);
+        CropData cropData1 = new CropData();
+        int landPrice = cropData1.calcLandCost;
+        System.out.println("How many acres of new land do you want to buy?");
+        
+        Scanner keyboard = new Scanner(System.in);
+        acresToBuy = keyboard.nextInt();
+        
+        if(acresToBuy < 1){
+            System.out.println("How many acres of new land do you want to buy?");
+            acresToBuy = keyboard.nextInt();
+        }
+        
+        
+        int wheatInStore = cropData1.getWheatInStore();
+        if((wheatInStore < (acresToBuy * landPrice)) || (wheatInStore < 0)){
+            System.out.println("Not enough wheat!");
+            System.out.println("How many acres of new land do you want to buy?");
+        }
     }
     
 }
